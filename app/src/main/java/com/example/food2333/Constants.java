86c50a8d498c8ab1fc743302c03fcdf5a24b6796
Ronnachai <ronnachai.bu.64@ubu.ac.th>
2022-09-22 18:56:10 +0700

    Food2

---
+++ app/src/main/java/com/example/food2333/Constants.java
@@ -0,0 +1,6 @@
+package com.example.food2333;
+
+public class Constants {
+    public static final String STORAGE_PATH_UPLOADS = "foods/";
+    public static final String DATABASE_PATH_UPLOADS = "foods";
+}
